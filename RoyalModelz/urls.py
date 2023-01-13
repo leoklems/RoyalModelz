9f467418f7db938257105143cb18615270e46934
leoklems <leoklems@yahoo.com>
2023-01-13 19:22:08 +0100

    deployment setup

--- RoyalModelz/urls.py
+++ RoyalModelz/urls.py
@@ -17,11 +17,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_store.urls')),
     path('verification/', include('verify_email.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
